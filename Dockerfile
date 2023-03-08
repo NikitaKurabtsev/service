c2fe8007c40c783e50047b319c6d35e42b5540c3
NikitaKurabtsev <nick3500hunna@gmail.com>
2023-03-08 22:41:53 +0300

    settings refactor

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
-FROM python:3.9-alpine3.16
+FROM python:3.9
 
-COPY requirements.txt /temp/requirements.txt
-COPY service /service
 WORKDIR /service
-EXPOSE 8000
 
-RUN apk add postgresql-client build-base postgresql-dev
+COPY requirements.txt /service/
+COPY . /service/
 
-RUN pip install -r /temp/requirements.txt
+RUN pip install -r /service/requirements.txt
 
 RUN adduser --disabled-password service-user
 
--- service/service/settings.py
+++ service/service/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from dotenv import load_dotenv
 from pathlib import Path
 import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%lvw8uzh6m(a@-3+(hs=5a79h)vq2*q@chvvk76-buq7k%-_37'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
