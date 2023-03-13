b345b51a5f93ee86db4f65753d3f84a09776a1ca
NikitaKurabtsev <nick3500hunna@gmail.com>
2023-03-13 14:50:40 +0100

    add optimization and annotation

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -8,10 +8,9 @@ from services.serializer import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.select_related('plan').prefetch_related(
-        Prefetch(
-            'client', queryset=Client.objects.select_related('user').only('company_name', 'user__email')
+    queryset = Subscription.objects.select_related('plan', 'service').prefetch_related(Prefetch(
+        'client', queryset=Client.objects.select_related('user').only('company_name', 'user__email')
         )
-    )
+    ).annotate(price=F('service__full_price') -
+                     F('service__full_price') * F('plan__discount_percent') / 100.00)
     serializer_class = SubscriptionSerializer
-
